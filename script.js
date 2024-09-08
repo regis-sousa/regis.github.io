7b3e497e85c3ac9adf9f13276398d25b250bab5e
regis <regiss.contato@gmail.com>
2024-09-08 17:34:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,60 @@
-let isFirefox = typeof InstallTrigger !== 'undefined';
-const words = "página em construção";
+const speed = 7;
+const r = gsap.timeline({ repeat: -1 });
+const o = gsap.timeline({ repeat: -1 });
+const h = gsap.timeline({ repeat: -1 });
 
-let ANGLE = 360;
-const ANIMATION_DURATION = 4000;
-
-const animation = () => {
-  ANGLE -= 1; // Incremento do ângulo
-  document.querySelectorAll(".spiral *").forEach((el, i) => {
-    
-    const translateY = Math.sin(ANGLE * (Math.PI / 120)) * 100;
-    const scale = Math.cos(ANGLE * (Math.PI / 120)) * 0.5 + 0.5;
-    
-    
-    const offset = parseInt(el.dataset.offset);
-    const delay = i * (ANIMATION_DURATION / 16) - offset;
-
-    setTimeout(() => {
-      el.style.transform = `translateY(${translateY}px) scale(${scale})`;
-    }, delay);
-  });
-
-  requestAnimationFrame(animation);
-};
-
-const characters = words.split("").forEach((char, i) => {
-  const createElement = (offset) => {
-    const div = document.createElement("div");
-    div.innerText = char;
-    div.classList.add("character");
-    div.setAttribute("data-offset", offset);
-    div.style.animationDelay = `-${i * (ANIMATION_DURATION / 16) - offset}ms`
-    return div;
-  };
-
-  document.querySelector("#spiral").append(createElement(0));
-  document
-    .querySelector("#spiral2")
-    .append(createElement((isFirefox ? 1 : -1) * (ANIMATION_DURATION / 2)));
+const $ticket = document.querySelector(".ticket");
+$ticket.addEventListener("mouseenter", () => {
+  r.pause();
+  o.pause();
+  h.pause();
+});
+$ticket.addEventListener("mouseleave", () => {
+  r.play();
+  o.play();
+  h.play();
 });
 
+r.to("#app", {
+  "--r": "180deg",
+  "--p": "0%",
+  duration: speed,
+  ease: "sine.in"
+});
+r.to("#app", {
+  "--r": "360deg",
+  "--p": "100%",
+  duration: speed,
+  ease: "sine.out"
+});
+o.to("#app", {
+  "--o": 1,
+  duration: speed / 2,
+  ease: "power1.in"
+});
+o.to("#app", {
+  "--o": 0,
+  duration: speed / 2,
+  ease: "power1.out"
+});
 
-// @property CSS doesn't work in Firefox, so it must be animated using JavaScript.
-if(isFirefox){
-  animation();
-}
-
+h.to("#app", {
+  "--h": "100%",
+  duration: speed / 2,
+  ease: "sine.in"
+});
+h.to("#app", {
+  "--h": "50%",
+  duration: speed / 2,
+  ease: "sine.out"
+});
+h.to("#app", {
+  "--h": "0%",
+  duration: speed / 2,
+  ease: "sine.in"
+});
+h.to("#app", {
+  "--h": "50%",
+  duration: speed / 2,
+  ease: "sine.out"
+});
